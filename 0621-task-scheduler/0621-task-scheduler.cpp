617bd42c3b5706ffcd03f1e6c5c0ab790d992dec
Sachin <sachin3434zzz@gmail.com>
2026-05-25 10:42:54 +0530

    Time: 26 ms (44.16%), Space: 47.8 MB (11.51%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        unordered_map<char,int> freq;
+        for(char task: tasks) freq[task]++;
+        priority_queue<int> maxHeap;
+        for(auto& entry: freq) maxHeap.push(entry.second);
+        int time=0;
+        while(!maxHeap.empty()) {
+            vector<int> temp;
+            int cycle=n+1;
+            int i=0;
+            while (i<cycle && !maxHeap.empty()) {
+                int cnt=maxHeap.top();
+                maxHeap.pop();
+                cnt--;
+                if (cnt>0) temp.push_back(cnt);
+                time++;
+                i++;
+            }
+            for(int val: temp) maxHeap.push(val);
+            if(maxHeap.empty()) break;
+            time+=(cycle-i);
+        }
+        return  time;
+    }
+};
