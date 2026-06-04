d157ec98d7b7039b406cff89df85cea8f7be2151
Sachin <sachin3434zzz@gmail.com>
2026-06-04 19:06:29 +0530

    Time: 52 ms (71.76%), Space: 21.2 MB (59.19%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -12,11 +12,13 @@ public:
         q.pop();
 
         if(word==endWord) return steps;
-
+        //word=hat
+        //word len*26
         for(int i=0;i<word.size();i++) {
             char original=word[i];
             for(char ch='a';ch<='z';ch++) {
                 word[i]=ch;
+                //it exist in the set
                 if(st.find(word)!=st.end()) {
                     st.erase(word);
                     q.push({word,steps+1});
