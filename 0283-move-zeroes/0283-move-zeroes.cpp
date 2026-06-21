14d046c59f34c951964ec5d3aa1ebebc44a7b4ee
Sachin <sachin3434zzz@gmail.com>
2026-06-21 11:26:47 +0530

    Time: 0 ms (100%), Space: 23.8 MB (55.01%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,30 +1,13 @@
 class Solution {
 public:
-  void moveZeroes(vector<int>& nums) {
-      int n = nums.size();
+    void moveZeroes(vector<int>& nums) {
+        int j = 0;  // position for next non-zero
 
-      // Count the zeroes
-      int numZeroes = 0;
-      for (int i = 0; i < n; i++) {
-          numZeroes += (nums[i] == 0);
-      }
-
-      // Make all the non-zero elements retain their original order.
-      vector<int> ans;
-      for (int i = 0; i < n; i++) {
-          if (nums[i] != 0) {
-              ans.push_back(nums[i]);
-          }
-      }
-
-      // Move all zeroes to the end
-      while (numZeroes--) {
-          ans.push_back(0);
-      }
-
-      // Combine the result
-      for (int i = 0; i < n; i++) {
-          nums[i] = ans[i];
-      }
-  }
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                swap(nums[i], nums[j]);
+                j++;
+            }
+        }
+    }
 };
