a222d761d0ba4982af61ce023405ddde909e5765
Sachin <sachin3434zzz@gmail.com>
2026-06-26 10:43:06 +0530

    Time: 0 ms (100%), Space: 12 MB (66.88%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.cpp
+++ 0682-baseball-game/0682-baseball-game.cpp
@@ -15,9 +15,14 @@ public:
             } else if(op=="C") {
                 stack.pop_back();
             } else {
-                stack.push_back(stoi(op));
+                stack.push_back(stoi(op)); //String TO Integer.
             }
         }
-        return accumulate(stack.begin(),stack.end(),0);
+        int sum = 0;
+        for (int x : stack) {
+            sum += x;
+        }
+        return sum;
+        // return accumulate(stack.begin(),stack.end(),0); //sum of all stack, initially sum is 0
     }
 };
